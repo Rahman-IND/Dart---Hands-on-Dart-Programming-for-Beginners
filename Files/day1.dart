97523d7f52c30cff10a0430f8a945610d5b730bc
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-08-26 21:11:35 +0530

    add Files folder

---
+++ Files/day1.dart
@@ -0,0 +1,28 @@
+//p1.dart
+
+//void main() {
+//   var firstname = "Motinur";
+//   var lastname = "Rahman";
+//   print("Welcome ${firstname} ${lastname} to Dart programming");
+// }
+
+//p2.dart
+
+//void main() {
+//   var fname = "Motinur";
+//   var lname = "Rahman";
+//   print("Welcome to dart programming");
+//   print("Your first name is ${fname}");
+//   print("Your last name is ${lname}");
+// }
+
+// p3.dart
+
+void main() {
+  var empName = "Motinur Rahman";
+  var empAge = "35";
+  var empSalary = "35000";
+  var empDept = "Accounts Dept";
+  print("Welcome to ABC corporation");
+  print("Your name is ${empName}, You are ${empAge} years old, Your salary is ${empSalary} and Your Department is ${empDept}");
+}
---
+++ Files/day2.dart
@@ -0,0 +1,19 @@
+// p4.dart
+
+void main() {
+  int empAge = 39;
+  double empSalary = 40000.69;
+  var empAddress = "b420/B, 2nd floor, abc path, xyz road, India";
+  bool empflag = true;
+  var emplist = [1, 3, 5, 7, 9];
+  var empdept = {'Dept1': 'Accounts', 'Dept2': 'IT', 'Dept3': 'Administration'};
+  var laughemoji = 'u\{1f600}';
+
+  print("Your is age is ${empAge}");
+  print("Your is Salary is ${empSalary}");
+  print("Your is address is ${empAddress}");
+  print("Your is flag is ${empflag}");
+  print("Your is list is ${emplist}");
+  print("Your is department is ${empdept}");
+  print("Your is emoji is ${laughemoji}");
+}
