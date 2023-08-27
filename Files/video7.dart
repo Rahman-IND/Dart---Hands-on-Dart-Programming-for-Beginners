a5e8a054f976fe3ca6c61083b8c95f1b683018fb
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-08-27 20:49:03 +0530

    add video7
    added 7th video's code to the file

---
+++ Files/video7.dart
@@ -0,0 +1,27 @@
+void main() {
+
+  //you can also declare and initilize the variable like:
+  // var var1 = 50;
+  // var var2 = 10;
+
+  var var1;
+  var var2;
+  var1 = 50;
+  var2 = 10;
+
+  var varSum, varSub, varMul, varDiv1, varDiv2, varMod;
+
+  varSum = var1 + var2;
+  varSub = var1 - var2;
+  varMul = var1 * var2;
+  varDiv1 = var1 / var2;
+  varDiv2 = var1 ~/ var2;
+  varMod = var1 % var2;
+
+  print('The sum is : ${varSum}');
+  print('The Subtraction is : ${varSub}');
+  print('The Multiplication is : ${varMul}');
+  print('The Divison is : ${varDiv1}');           //with decimal values
+  print('The Divison is : ${varDiv2}');           //without decimal values
+  print('The Modulo is : ${varMod}');             //to get remainder
+}
