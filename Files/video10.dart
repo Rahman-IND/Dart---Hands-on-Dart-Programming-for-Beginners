2acc3151d1931ce14e8790028a6e1b192086880a
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-08-28 19:22:14 +0530

    add video10
    added 10th video's code in the file

---
+++ Files/video10.dart
@@ -0,0 +1,20 @@
+void main() {
+  var num1 = 50;
+  var num2 = 40;
+  print('The value of num1 is : ${num1} and num2 is : ${num2}\n');
+  num1 += num2;
+  print('The value of num1 += num2 is :${num1}');
+  print('The current value of num1 is : ${num1} and num2 is : ${num2}\n');
+  num1 -= num2;
+  print('The value of num1 -= num2 is :${num1}');
+  print('The current value of num1 is : ${num1} and num2 is : ${num2}\n');
+  num1 *= num2;
+  print('The value of num1 *= num2 is :${num1}');
+  print('The value of num1 is : ${num1} and num2 is : ${num2}\n');
+  num1 ~/= num2;
+  print('The value of num1 ~/= num2 is :${num1}');
+  print('The value of num1 is : ${num1} and num2 is : ${num2}\n');
+  num1 %= num2;
+  print('The value of num1 %= num2 is :${num1}');
+  print('The value of num1 is : ${num1} and num2 is : ${num2}');
+}
