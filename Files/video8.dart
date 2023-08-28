02c987b28f43478595ade0101f0cc892b9ef05c7
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-08-28 14:18:08 +0530

    add video8
    added 5th video's code  to the file

---
+++ Files/video8.dart
@@ -0,0 +1,13 @@
+// part 1
+
+void main() {
+  var var1, var2;
+  var1 = 50;
+  var2 = 10;
+  print('The Addition is : ${var1 + var2}');
+  print('The Subtraction is : ${var1 - var2}');
+  print('The Multiplication is : ${var1 * var2}');
+  print('The Division is : ${var1 / var2}');
+  print('The Division is : ${var1 ~/ var2}');
+  print('The Modulo is : ${var1 % var2}');
+}
