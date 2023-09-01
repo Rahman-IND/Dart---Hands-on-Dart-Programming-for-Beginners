775e777f1f657c0bf9d604eda91bc7913560a0a8
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-01 19:45:39 +0530

    add changes
    added output statement changes in the code

--- Files/video14.dart
+++ Files/video14.dart
@@ -1,3 +1,5 @@
+//Topic: Bitwise Operators
+
 void main() {
   var var1, var2, result;
   var1 = 42;
@@ -8,10 +10,10 @@ void main() {
   print('value of var1 : ${var1} | var2 : ${var2} is : ${result}');
   result = var1 ^ var2;
   print('value of var1 : ${var1} ^ var2 : ${var2} is : ${result}');
-  result = ~ var1;
-  print('value of var1 : ${var1} & var2 : ${var2} is : ${result}');
+  result = ~var1;
+  print("value of var1 : ${var1} and it's ~ inverse binary result is : ${result}");
   result = var1 << 3;
-  print('value of var1 : ${var1} << var2 : ${var2} is : ${result}');
+  print("value of var1 is ${var1} and after 3 left shift, it's binary result is : ${result}");
   result = var1 >> 3;
-  print('value of var1 : ${var1} >> var2 : ${var2} is : ${result}');
+  print("value of var1 is ${var1} and after 3 right shift, it's binary result is : ${result}");
 }
