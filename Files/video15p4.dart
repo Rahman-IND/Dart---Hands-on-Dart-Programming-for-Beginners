15db6f170010f0a01e6167f1deefd844efc9763b
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-01 21:39:39 +0530

    add video15p4
    added 15th video part 4's code in the file

---
+++ Files/video15p4.dart
@@ -0,0 +1,8 @@
+//Topic: Conditional Operators Or Ternary operator (??)
+
+void main() {
+  var num1 = null;
+  var num2 = 4;
+  var result = (num1 ?? num2);
+  print("The result is : ${result}");
+}
