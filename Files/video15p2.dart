5d80bc421a1491c83ad411f0afd84a9af9448f29
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-01 20:11:42 +0530

    add video15p2
    added 15th video part 2's code in the file

---
+++ Files/video15p2.dart
@@ -0,0 +1,9 @@
+//Topic: Conditional Operators Or Ternary operator (?)
+
+void main() {
+  int num1 = 40;
+  int num2 = 12;
+  var result;
+  result = (num1 > num2) ? "HigherNumber" : "SmallerNumber";
+  print('The result is : ${result}');
+}
