e081f6b27d9eb8be7ded6cd41c7d5dd38ea8c42b
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-01 19:46:34 +0530

    add video15
    added 15th video's code in the file

---
+++ Files/video15.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main() {
+  var var1, var2, result;
+  int intvar1, intvar2;
+
+  print('Enter the first value: ');
+  var1 = stdin.readLineSync();
+  intvar1 = int.parse(var1);
+
+  print('Enter the second value: ');
+  var2 = stdin.readLineSync();
+  intvar2 = int.parse(var2);
+
+  result = intvar1 & intvar2;
+  print('The value of intvar1 : ${intvar1} & intvar2 :${intvar2} is : ${result}');
+  result = intvar1 | intvar2;
+  print('The value of intvar1 : ${intvar1} | intvar2 :${intvar2} is : ${result}');
+  result = intvar1 ^ intvar2;
+  print('The value of intvar1 : ${intvar1} ^ intvar2 :${intvar2} is : ${result}');
+  result = ~ intvar1;
+  print("value of var1 is ${var1} and it's ~ inverse binary result is : ${result}");
+  result = intvar1 << 3;
+  print("value of var1 is ${var1} and after 3 left shift, it's binary result is : ${result}");
+  result = intvar1 >> 3;
+  print("value of var1 is ${var1} and after 3 right shift, it's binary result is : ${result}");
+}
