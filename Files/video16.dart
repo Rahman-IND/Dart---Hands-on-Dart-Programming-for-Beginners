21bfdc8718b8ff295d7235817ba65e053ae2cbe2
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-02 21:07:29 +0530

    add video16
    added 16th video's code in the file

---
+++ Files/video16.dart
@@ -0,0 +1,14 @@
+//Topic: Dart constants
+
+void main() {
+  final varint1 = 50;
+  final varint2 = 100;
+  const pi = 3.14;
+  int radius = 55;
+  double result = pi * radius * radius;
+
+  print('The value of final variable varint1 is ${varint1}');
+  print('The value of final variable varint2 is ${varint2}');
+  print('The value of constant variable pi is ${pi}');
+  print('The value of result is ${result}');
+}
