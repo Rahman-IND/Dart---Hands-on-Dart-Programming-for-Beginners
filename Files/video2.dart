fd40b418d67f669bef4a12ccaf6ea20af4cc68b3
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-07 13:52:49 +0530

    add chage in video2
    added changes in video2.dart file

--- Files/video2.dart
+++ Files/video2.dart
@@ -1,28 +1,18 @@
-//p1.dart
-
-//void main() {
-//   var firstname = "Motinur";
-//   var lastname = "Rahman";
-//   print("Welcome ${firstname} ${lastname} to Dart programming");
-// }
-
-//p2.dart
-
-//void main() {
-//   var fname = "Motinur";
-//   var lname = "Rahman";
-//   print("Welcome to dart programming");
-//   print("Your first name is ${fname}");
-//   print("Your last name is ${lname}");
-// }
-
-// p3.dart
+/*variable initialization & declaration and printing output*/
 
 void main() {
-  var empName = "Motinur Rahman";
-  var empAge = "35";
-  var empSalary = "35000";
-  var empDept = "Accounts Dept";
-  print("Welcome to ABC corporation");
-  print("Your name is ${empName}, You are ${empAge} years old, Your salary is ${empSalary} and Your Department is ${empDept}");
+  var firstname = "Priyanka";
+  var lastname = "Sharma";
+  print("\n Welcome ${firstname} ${lastname} to Dart Programming");
 }
+
+// // p3.dart
+
+// void main() {
+//   var empName = "Motinur Rahman";
+//   var empAge = "35";
+//   var empSalary = "35000";
+//   var empDept = "Accounts Dept";
+//   print("Welcome to ABC corporation");
+//   print("Your name is ${empName}, You are ${empAge} years old, Your salary is ${empSalary} and Your Department is ${empDept}");
+// }
