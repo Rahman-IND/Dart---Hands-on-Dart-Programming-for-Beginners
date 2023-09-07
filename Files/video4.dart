ef32f99f1a363efa32db63f123dc6b18b72dd4c5
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-07 17:29:46 +0530

    add changes in Video4.dart

--- Files/video4.dart
+++ Files/video4.dart
@@ -1,19 +1,19 @@
-// p4.dart
+/*Topic : Different data types and their keywords */
 
 void main() {
-  int empAge = 39;
-  double empSalary = 40000.69;
-  var empAddress = "b420/B, 2nd floor, abc path, xyz road, India";
-  bool empflag = true;
-  var emplist = [1, 3, 5, 7, 9];
-  var empdept = {'Dept1': 'Accounts', 'Dept2': 'IT', 'Dept3': 'Administration'};
-  var laughemoji = 'u\{1f600}';
+  int empAge = 25;
+  double empSalary = 25000.96;
+  var empAddress = "abc/2nd floor, xyz road";
+  bool empFlag = true;
+  var empList = [1, 3, 5, 7, 9];
+  var empDept={'Dept1':'IT','Dept2':'Accounts','Dept3':'Sales','Dept4':'Administration'};
+  var laughEmoji="u\1f600";
 
-  print("Your is age is ${empAge}");
-  print("Your is Salary is ${empSalary}");
-  print("Your is address is ${empAddress}");
-  print("Your is flag is ${empflag}");
-  print("Your is list is ${emplist}");
-  print("Your is department is ${empdept}");
-  print("Your is emoji is ${laughemoji}");
+  print('Your age is ${empAge}');
+  print('Your Salary is ${empSalary}');
+  print('Your Address is ${empAddress}');
+  print('Your Flag is ${empFlag}');
+  print('Your List is ${empList}');
+  print('Your Department are ${empDept}');
+  print('Your Emoji is ${laughEmoji}');
 }
