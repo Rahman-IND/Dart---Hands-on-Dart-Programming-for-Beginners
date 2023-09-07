d33386737e504bbca2566fb2e57e7fa8fd6259f6
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-07 13:54:54 +0530

    add video2p2
    added 2nd video's part 2

---
+++ Files/video2p2.dart
@@ -0,0 +1,8 @@
+/*variable initialization & declaration and printing output in different lines*/
+void main() {
+  var fname = "Priyanka";
+  var lname = "Sharma";
+  print("\n Welcome to Dart Programming");
+  print('Your First name is : ${fname}');
+  print('Your last name is : ${lname}');
+}
