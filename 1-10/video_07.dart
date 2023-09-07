ee6381eae26bec38def401b2c803107ffb4318d7
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-07 23:56:49 +0530

    add changes to video_07.dart

--- 1-10/video_07.dart
+++ 1-10/video_07.dart
@@ -1,27 +1,22 @@
+/*Topic : Operators (arithmetic operators) */
+
 void main() {
-
-  //you can also declare and initilize the variable like:
-  // var var1 = 50;
-  // var var2 = 10;
-
-  var var1;
-  var var2;
-  var1 = 50;
-  var2 = 10;
+  var num1 = 50;
+  var num2 = 10;
 
   var varSum, varSub, varMul, varDiv1, varDiv2, varMod;
 
-  varSum = var1 + var2;
-  varSub = var1 - var2;
-  varMul = var1 * var2;
-  varDiv1 = var1 / var2;
-  varDiv2 = var1 ~/ var2;
-  varMod = var1 % var2;
+  varSum = num1 + num2;
+  varSub = num1 - num2;
+  varMul = num1 * num2;
+  varDiv1 = num1 / num2;
+  varDiv2 = num1 ~/ num2;
+  varMod = num1 % num2;
 
-  print('The sum is : ${varSum}');
+  print('The Addition is : ${varSum}');
   print('The Subtraction is : ${varSub}');
   print('The Multiplication is : ${varMul}');
-  print('The Division is : ${varDiv1}');           //with decimal values
-  print('The Division is : ${varDiv2}');           //without decimal values
-  print('The Modulo is : ${varMod}');             //to get remainder
+  print('The Division is : ${varDiv1}');
+  print('The Division is : ${varDiv2}');
+  print('The Modulo is : ${varMod}');
 }
