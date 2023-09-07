30350a354c4c271aeecd664863e05307824a9b4a
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-07 16:54:29 +0530

    add video2p3.dart
    added 2nd video's part 3 into the file

---
+++ Files/video2p3.dart
@@ -0,0 +1,11 @@
+/*Topic: variable initialization & declaration 
+and printing different data types in output*/
+
+void main() {
+  var empName = "Priyanka Sharma";
+  var empAge = "25";
+  var empSalary = "25000";
+  var empDept = "Accounts Dept";
+  print("\n Welcome to ABC Corporation");
+  print(" Your Name is ${empName}, you are ${empAge} Years old, your Salary is ${empSalary} and your Department is ${empDept}");
+}
