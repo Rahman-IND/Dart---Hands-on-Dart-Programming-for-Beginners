eccedc3f326f7b48fe3745b73458d6e2d4db46fa
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-07 16:53:24 +0530

    add changes in video2.dart
    added required changes into the file

--- Files/video2.dart
+++ Files/video2.dart
@@ -1,18 +1,7 @@
-/*variable initialization & declaration and printing output*/
+/*Topic: variable initialization & declaration and printing output*/
 
 void main() {
   var firstname = "Priyanka";
   var lastname = "Sharma";
   print("\n Welcome ${firstname} ${lastname} to Dart Programming");
 }
-
-// // p3.dart
-
-// void main() {
-//   var empName = "Motinur Rahman";
-//   var empAge = "35";
-//   var empSalary = "35000";
-//   var empDept = "Accounts Dept";
-//   print("Welcome to ABC corporation");
-//   print("Your name is ${empName}, You are ${empAge} years old, Your salary is ${empSalary} and Your Department is ${empDept}");
-// }
