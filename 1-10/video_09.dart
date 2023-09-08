2dfb85bd45e83b735460ba75644ea9db63f89a99
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-08 17:08:12 +0530

    Add changes

--- 1-10/video_09.dart
+++ 1-10/video_09.dart
@@ -1,19 +1,22 @@
+/*Topic : Arithmetic operators with Double variable and user input */
+
 import 'dart:io';
 
 void main() {
   var var1, var2;
-  double dvar1, dvar2;
-  print('Enter the first number: ');
-  var1 = stdin.readLineSync();
-  dvar1 = double.parse(var1);
-  print('Enter the second number: ');
-  var2 = stdin.readLineSync();
-  dvar2 = double.parse(var2);
+  double Dnum1, Dnum2;
 
-  print('The addition is : ${dvar1 + dvar2}');
-  print('The subtraction is : ${dvar1 - dvar2}');
-  print('The multiplication is : ${dvar1 * dvar2}');
-  print('The division is : ${dvar1 / dvar2}');
-  print('The division is : ${dvar1 ~/ dvar2}');
-  print('The modulo is : ${dvar1 % dvar2}');
+  print("Enter the first variable : ");
+  var1 = stdin.readLineSync();
+  Dnum1 = double.parse(var1);
+  print("Enter the Second variable : ");
+  var2 = stdin.readLineSync();
+  Dnum2 = double.parse(var2);
+
+  print("The result of Addition is : ${Dnum1 + Dnum2}");
+  print("The result of Subtraction is : ${Dnum1 - Dnum2}");
+  print("The result of Multiplication is : ${Dnum1 * Dnum2}");
+  print("The result of Division is : ${Dnum1 / Dnum2}");
+  print("The result of Division is : ${Dnum1 ~/ Dnum2}");
+  print("The result of modulo is : ${Dnum1 % Dnum2}");
 }
