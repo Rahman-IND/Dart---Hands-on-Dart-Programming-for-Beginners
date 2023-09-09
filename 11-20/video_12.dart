de61132358bd528656ab738536ede000e7d4f019
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-09 14:52:25 +0530

    add changes to video_12.dart

--- 11-20/video_12.dart
+++ 11-20/video_12.dart
@@ -1,13 +1,15 @@
+/* Topic : Logical Operators (boolean logical operators)*/
+
 void main() {
   bool varbool1, varbool2;
   varbool1 = true;
   varbool2 = false;
   var result;
-  
+
   result = (varbool1 && varbool2);
-  print('when varbool1 = true && varbool2 = true then result is :' +result.toString());
+  print("When varbool1 = true && varbool2 = false then result is :" + result.toString());
   result = (varbool1 || varbool2);
-  print('when varbool1 = true || varbool2 = true then result is :' +result.toString());
+  print("When varbool1 = true || varbool2 = false then result is :" + result.toString());
   result = !(varbool1 && varbool2);
-  print('when varbool1 = true NOT && varbool2 = true then result is :' +result.toString());
+  print("When varbool1 = true NOT && varbool2 = false then result is :" + result.toString());
 }
