5339756ecc0f5744a3bf251a710b9b57e9aac5bd
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-09 15:45:49 +0530

    add changes to video_13.dart

--- 11-20/video_13.dart
+++ 11-20/video_13.dart
@@ -1,18 +1,21 @@
+/* Topic : Logical Operators with user-input and conditional statements */
+
 import 'dart:io';
 
 void main() {
   var var1, var2;
   int num1, num2;
-  bool varbool1;
+  bool result;
 
-  print('Enter the first number: ');
+  print("Enter the first number : ");
   var1 = stdin.readLineSync();
   num1 = int.parse(var1);
-  print('Enter the second number: ');
+  print("Enter the second number : ");
   var2 = stdin.readLineSync();
   num2 = int.parse(var2);
-  print('The value of num1 is = ${num1} and num2 is = ${num2} \n');
-  varbool1 = (num1 > num2);
-  print('If num1 > num2 then True or eles False \n');
-  print('Result is :' +varbool1.toString());
+
+  print("The value of num1 is : ${num1} and num2 is : ${num2} \n");
+  result = (num1 > num2);
+  print("If num1 = ${num1} > num2 = ${num2} then 'True' or else 'False' \n");
+  print("The result is : " + result.toString());
 }
