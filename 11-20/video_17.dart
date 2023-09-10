1e5818a386ad197e0b35c010a444c417a8078e49
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-10 20:00:46 +0530

    add changes to video_17.dart

--- 11-20/video_17.dart
+++ 11-20/video_17.dart
@@ -1,36 +1,35 @@
-//Topic: method and properties of int and double variable
-
+/*Topic : Method and Properties of Integer and Double variable */
 import 'dart:io';
 
 void main() {
   var num1, num2;
-  int varnum;
-  double vard;
+  int intnum;
+  double dnum;
 
-  print('Enter the value for integer variable : ');
+  print("Enter the Integer value : ");
   num1 = stdin.readLineSync();
-  varnum = int.parse(num1);
-  print('Enter the value for double varibale : ');
+  intnum = int.parse(num1);
+  print("Enter the Double value : ");
   num2 = stdin.readLineSync();
-  vard = double.parse(num2);
+  dnum = double.parse(num2);
 
-  print('The integer value entered was ${varnum}');
-  print('The double value entered was ${vard}');
-  print('The HashCode property is ${varnum.hashCode}');
-  print('The isFinite property is ${varnum.isFinite}');
-  print('The isInFinite property is ${varnum.isInfinite}');
-  print('The isNotaNumber property is ${varnum.isNaN}');
-  print('The isNegative property is ${varnum.isNegative}');
-  print('The Sign property is ${varnum.sign}');
-  print('The isEven property is ${varnum.isEven}');
-  print('The isOdd property is ${varnum.isOdd}\n');
+  print("The value of Integer variable is : ${intnum}");
+  print("The HashCode property is : ${intnum.hashCode}");
+  print("The isFinite property is : ${intnum.isFinite}");
+  print("The isInfinite property is : ${intnum.isInfinite}");
+  print("The Sign property is : ${intnum.sign}");
+  print("The isNaN property is : ${intnum.isNaN}");
+  print("The isNegative property is : ${intnum.isNegative}");
+  print("The isOdd property is : ${intnum.isOdd}");
+  print("The isEven property is : ${intnum.isEven}\n");
 
-  print('The absolute method :${vard.abs()}');
-  print('The ceil method :${vard.ceil()}');
-  print('The Floor method :${vard.floor()}');
-  print('The Compareto method :${vard.compareTo(56.66)}');
-  print('The remainder method :${vard.remainder(2)}');
-  print('The round method :${vard.round()}');
-  print('The toInt method :${vard.toInt()}');
-  print('The truncate method :${vard.truncate()}');
-  }
+  print("The value of Double variable is : ${dnum}");
+  print("The Absolute method is : ${dnum.abs()} ");
+  print("The Ceil method is : ${dnum.ceil()}");
+  print("The Floor method is : ${dnum.floor()}");
+  print("The Round method is : ${dnum.round()}");
+  print("The toInt method is : ${dnum.toInt()}");
+  print("The Compareto method is : ${dnum.compareTo(55.66)}");
+  print("The remainder method is : ${dnum.remainder(2)}");
+  print("The Truncate method is : ${dnum.truncate()}"); 
+}
