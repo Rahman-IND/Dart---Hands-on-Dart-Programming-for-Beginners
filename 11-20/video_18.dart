aadf8bc135ea546208fbd1e26b6e74407a2f678d
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-10 22:11:00 +0530

    add changes to video_18.dart

--- 11-20/video_18.dart
+++ 11-20/video_18.dart
@@ -1,25 +1,25 @@
-// topic: String methods (use of concatenation operator)
-
+/*Topic : String - method and use of concatenation operator(+)*/
 import 'dart:io';
 
 void main() {
   var fname, lname, address;
-  print("Enter your first name: ");
+
+  print("Enter the first name : ");
   fname = stdin.readLineSync();
-  print("Enter your last name: ");
+  print("Enter the last name : ");
   lname = stdin.readLineSync();
-  print("Enter your address: ");
+  print("Enter your Address :  ");
   address = stdin.readLineSync();
 
-  print('Your name is : ' + fname + ' ' + lname);
+  print("Your name is : " + fname + ' ' + lname);
   print("Your address is : " + address);
 
   String name = fname + ' ' + lname;
-  
-  print("toLowerCase method: ${name.toLowerCase()}");
-  print('toUpperCase method: ${name.toUpperCase()}');
-  print('trim method: ${name.trim()}');
-  print('comapareTo method: ${name.compareTo('Priyanka')}');
-  print('replaceAll method: ${name.replaceAll('yan', 'yaan')}');
-  print('substring method: ${name.substring(4)}');
+
+  print("toLowerCase method : ${name.toLowerCase()}");
+  print("toUpperCase method : ${name.toUpperCase()}");
+  print("trim method : ${name.trim()}");
+  print("compareTo method : ${name.compareTo('Priyanka')}");
+  print("replaceAll method : ${name.replaceAll('yan', 'yaan')}");
+  print("substring method : ${name.substring(4)}");
 }
