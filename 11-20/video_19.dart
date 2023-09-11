c36013da8be96f6364f22b431d9b2f174b0822b8
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-11 22:00:21 +0530

    Add changes to video_19.dart

--- 11-20/video_19.dart
+++ 11-20/video_19.dart
@@ -1,13 +1,14 @@
-//Topic: List (with fixed array size 7 )(with out user input)
+/*Topic: List with fixed size 7 and without user-input*/
+
 void main() {
-  List<String> weekdays = List.filled(7, '');
-  weekdays[0] = 'Monday';
-  weekdays[1] = 'Tuesday';
-  weekdays[2] = 'Wednesday';
-  weekdays[3] = 'Thursday';
-  weekdays[4] = 'Friday';
-  weekdays[5] = 'Saturday';
-  weekdays[6] = 'Sunday';
-  print('The values of weekdays list are:');
-  print(weekdays);
+  List<String> Weekdays = List.filled(7, '');
+  Weekdays[0] = 'Monday';
+  Weekdays[1] = 'Tuesday';
+  Weekdays[2] = 'Wednesday';
+  Weekdays[3] = 'Thursday';
+  Weekdays[4] = 'Friday';
+  Weekdays[5] = 'Saturday';
+  Weekdays[6] = 'Sunday';
+  print("The values of weekdays list are : ");
+  print(Weekdays);
 }
