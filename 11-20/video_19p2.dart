b93d7bfeb42607c15f30ace65bc067e23ff0890e
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-11 22:17:22 +0530

    add changes to video_19p2.dart

--- 11-20/video_19p2.dart
+++ 11-20/video_19p2.dart
@@ -1,27 +1,26 @@
-//Topic: List with user input (with fixed array size 7 )
-
+/*Topic: List with fixed size 7 and with user-input*/
 import 'dart:io';
 
 void main() {
   List<String?> Weekdays = List.filled(7, '');
 
-  print('enter the first value of the list : ');
+  print("Enter the first value of the list : ");
   Weekdays[0] = stdin.readLineSync();
-  print('enter the second value of the list : ');
+  print("Enter the second value of the list : ");
   Weekdays[1] = stdin.readLineSync();
-  print('enter the third value of the list : ');
+  print("Enter the third value of the list : ");
   Weekdays[2] = stdin.readLineSync();
-  print('enter the fourth value of the list : ');
+  print("Enter the forth value of the list : ");
   Weekdays[3] = stdin.readLineSync();
-  print('enter the fifth value of the list : ');
+  print("Enter the fifth value of the list : ");
   Weekdays[4] = stdin.readLineSync();
-  print('enter the sixth value of the list : ');
+  print("Enter the sixth value of the list : ");
   Weekdays[5] = stdin.readLineSync();
-  print('enter the seventh value of the list : ');
+  print("Enter the seventh value of the list : ");
   Weekdays[6] = stdin.readLineSync();
-  print("The values of the List are : ");
+  print("The value of the list is : ");
   print(Weekdays);
-  print('The length of the List is ' + Weekdays.length.toString());
-  print('The first value of the list is : '+ Weekdays.first!);
-  print('The last value of the list is : ' + Weekdays.last!);
+  print("The length of the list is : " + Weekdays.length.toString());
+  print("The first value of the list is : " + Weekdays.first!);
+  print("The last value of the list is : " + Weekdays.last!);
 }
